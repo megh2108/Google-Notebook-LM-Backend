4e78c0b39e5fbdc216dcf50a60e3c642b37fdc8c
megh2108 <20cp041@bvmengineering.ac.in>
2025-08-15 00:07:12 +0530

    coorected cors

--- api/config/corsConfig.js
+++ api/config/corsConfig.js
@@ -1,7 +1,10 @@
 const cors = require("cors");
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: [
+    "http://localhost:5173",
+    "https://google-notebook-lm-frontend.vercel.app"
+  ],
   methods: ["GET", "POST", "PUT", "DELETE"],
 };
 
